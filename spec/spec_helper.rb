9179e90ef4152cb436c8ec6547b69d4eaa9040a0
Vincent Robert <vincent.robert@nanego.com>
2020-12-16 11:02:22 +0100

    Add helper methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,3 +78,23 @@ def log_user(login, password)
   end
   expect(current_path).to eq '/my/page'
 end
+
+def assert_mail_body_match(expected, mail, message=nil)
+  if expected.is_a?(String)
+    expect(mail_body(mail)).to include(expected)
+  else
+    assert_match expected, mail_body(mail), message
+  end
+end
+
+def assert_mail_body_no_match(expected, mail, message=nil)
+  if expected.is_a?(String)
+    expect(mail_body(mail)).to_not include expected
+  else
+    assert_no_match expected, mail_body(mail), message
+  end
+end
+
+def mail_body(mail)
+  mail.parts.first.body.encoded
+end
