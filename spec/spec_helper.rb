ff788b15f0073ce498802d6b6c97910cdf843798
Vincent <contact@vincent-robert.com>
2024-11-14 16:12:33 +0100

    Use capybara's has_current_path? method to wait for the page to load before checking the path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,7 +89,7 @@ def log_user(login, password)
     fill_in 'password', with: password
     find('input[name=login]').click
   end
-  expect(current_path).to eq '/my/page'
+  expect(page).to have_current_path('/my/page', wait: true)
 end
 
 def assert_mail_body_match(expected, mail, message = nil)
