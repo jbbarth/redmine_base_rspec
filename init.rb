b9c330c882439ceedab80fa88124c59fa44b754d
Vincent <contact@vincent-robert.com>
2024-11-14 14:59:20 +0100

    Add a specific patch to fix GitHub Workflows error with Redmine 6.0.1

--- init.rb
+++ init.rb
@@ -6,3 +6,33 @@ Redmine::Plugin.register :redmine_base_rspec do
   url 'https://github.com/jbbarth/redmine_base_rspec'
   author_url 'jeanbaptiste.barth@gmail.com'
 end
+
+# Specific patch for Redmine 6.0.1
+# Patch core helper method to temporary prevent a bug with GitHub Action Workflows on Redmine 6.0
+module BaseRspec
+  module IconsHelperPatch
+    def principal_icon(principal, **options)
+
+      if principal.is_a?(String)
+        puts "Error in principal_icon: First argument has to be a Principal, was the String #{principal.inspect}"
+        return nil
+      end
+
+      raise ArgumentError, "First argument has to be a Principal, was #{principal.inspect}" unless principal.is_a?(Principal)
+
+      principal_class = principal.class.name.downcase
+      sprite_icon('group', **options) if ['groupanonymous', 'groupnonmember', 'group'].include?(principal_class)
+    end
+  end
+end
+
+module Hooks
+  class ModelHook < Redmine::Hook::Listener
+    def after_plugins_loaded(_context = {})
+      if Redmine::VERSION::MAJOR >= 6
+        IconsHelper.prepend BaseRspec::IconsHelperPatch
+        ActionView::Base.prepend IconsHelper
+      end
+    end
+  end
+end
